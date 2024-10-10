94c1ebcc67a02e66ed7e13f955555991168248ef
ram <ram@ionia.io>
2024-10-10 17:52:46 +0530

    Added code for Latest

--- src/modules/acl/acl.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ACLController } from './acl.controller';
-import { ACLService } from './acl.service';
-
-@Module({
-  controllers: [ACLController],
-  providers: [ACLService],
-})
-export class AclModule {}
--- src/modules/acl/dto/update-acl.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAclDto } from './create-acl.dto';
-
-export class UpdateAclDto extends PartialType(CreateAclDto) {}
